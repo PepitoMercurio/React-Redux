52ab946d1e11f1f824963e35ecd5ae55f520e28b
Pepito Mercurio <coco.run91@gmail.com>
2024-02-12 14:29:17 +0100

    aa

--- src/pages/HomePage/homePage.jsx
+++ src/pages/HomePage/homePage.jsx
@@ -7,6 +7,7 @@ import store from "../../store";
 export default function HomePage() {
     return (
         <div>
+            <p>aaa</p>
             <Header />
             <Products />
             <Footer />
